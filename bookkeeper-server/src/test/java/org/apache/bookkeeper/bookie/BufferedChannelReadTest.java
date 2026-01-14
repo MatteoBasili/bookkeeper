5dbc52fa62af69d6fc07473421c1247b31aa9225
MatteoBasili <matthew2000.mb@gmail.com>
2026-01-14 18:00:00 +0100

    Aggiunto terzo test read BufferedChannel dopo Ba-dua

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelReadTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BufferedChannelReadTest.java
@@ -93,7 +93,6 @@ public class BufferedChannelReadTest {
                     // -------------------- Aggiunti dopo l'analisi con Jacoco -------------------- //
                     Arguments.of(invalidAllocatorInstance, null, emptyByteBuf(), BC_FC_CONTENT.length(), 1, null),                             // J-R1: Superato
                     Arguments.of(invalidAllocatorInstance, null, emptyByteBuf(), 0, BC_FC_CONTENT.length(), null)                             // J-R2: Superato
-
             );
 
         } catch (IOException e) {
@@ -248,11 +247,16 @@ public class BufferedChannelReadTest {
                     new BufferedChannelInstance(unpooledByteBufAllocator(),
                             validFileChannel(), 256, 256, 128);
 
+            BufferedChannelInstance notEnoughReadCapacityInstance =
+                    new BufferedChannelInstance(unpooledByteBufAllocator(),
+                            validFileChannel(), 256, BC_FC_CONTENT.length() - 1, 128);
+
             // Parametri: istanza, contenutoWriteBuffer, destPrimaRead, posPrimaRead, lengthPrimaRead,
             //            destSecondaRead, posSecondaRead, lengthSecondaRead
             return Stream.of(
-                    Arguments.of(validInstance, null, emptyByteBuf(), 1, BC_FC_CONTENT.length() - 1, emptyByteBuf(), 0, BC_FC_CONTENT.length()),            // B-R1: Superato
-                    Arguments.of(validInstance, null, emptyByteBuf(), 1, BC_FC_CONTENT.length() - 1, emptyByteBuf(), 1, BC_FC_CONTENT.length() - 1)            // B-R2: Superato
+                    Arguments.of(validInstance, null, emptyByteBuf(), 1, BC_FC_CONTENT.length() - 1, emptyByteBuf(), 0, BC_FC_CONTENT.length()),                // B-R1: Superato
+                    Arguments.of(validInstance, null, emptyByteBuf(), 1, BC_FC_CONTENT.length() - 1, emptyByteBuf(), 1, BC_FC_CONTENT.length() - 1),            // B-R2: Superato
+                    Arguments.of(notEnoughReadCapacityInstance, null, emptyByteBuf(), 0, BC_FC_CONTENT.length(), emptyByteBuf(), 0, BC_FC_CONTENT.length())     // B-R3: Superato
             );
         } catch (IOException e) {
             throw new RuntimeException("Errore nella preparazione dei casi di test", e);
